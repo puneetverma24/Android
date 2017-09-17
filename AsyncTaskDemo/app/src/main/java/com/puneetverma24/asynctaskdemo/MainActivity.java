6de522e305a8e574490eb3f3c873c3a474207895
Puneet Verma <puneetverm24@gmail.com>
2017-09-18 00:25:05 +0530

    Async task

--- AsyncTaskDemo/app/src/main/java/com/puneetverma24/asynctaskdemo/MainActivity.java
+++ AsyncTaskDemo/app/src/main/java/com/puneetverma24/asynctaskdemo/MainActivity.java
@@ -4,13 +4,18 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    TextView txt;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        txt=(TextView)findViewById(R.id.txt);
+
         new TestAsync().execute();
     }
 
@@ -21,10 +26,13 @@ public class MainActivity extends AppCompatActivity {
         protected void onPreExecute (){
             super.onPreExecute();
             Log.d(TAG + " PreExceute","On pre Exceute......");
+
+            Toast.makeText(MainActivity.this, "On pre Exceute", Toast.LENGTH_SHORT).show();// can access UI
         }
 
         protected String doInBackground(Void...arg0) {
             Log.d(TAG + " DoINBackGround","On doInBackground...");
+
 
             for(int i=0; i<10; i++){
                 Integer in = new Integer(i);
@@ -35,12 +43,15 @@ public class MainActivity extends AppCompatActivity {
 
         protected void onProgressUpdate(Integer...a){
             super.onProgressUpdate(a);
+
+            Toast.makeText(MainActivity.this, "onProgressUpdate" +a[0], Toast.LENGTH_SHORT).show();// can access UI
             Log.d(TAG + " onProgressUpdate", "You are in progress update ... " + a[0]);
         }
 
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
             Log.d(TAG + " onPostExecute", "" + result);
+            Toast.makeText(MainActivity.this, "onPostExecute", Toast.LENGTH_SHORT).show();// can access UI
         }
     }
 
