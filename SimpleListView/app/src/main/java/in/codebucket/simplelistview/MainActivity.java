f2cafce02bf8cec815526f0989503466eff141df
Puneet Verma <puneetverm24@gmail.com>
2018-06-22 10:28:27 +0530

    Added listView Please enter the commit message for your changes. Lines starting

---
+++ SimpleListView/app/src/main/java/in/codebucket/simplelistview/MainActivity.java
@@ -0,0 +1,38 @@
+package in.codebucket.simplelistview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final String[] data={"a","b","c","d","e","f","g","i","j"};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ListView list = findViewById(R.id.list);
+
+        ArrayAdapter<String> adapter= new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, data);
+        list.setAdapter(adapter);
+
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Position : "+position, Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+
+
+
+
+    }
+}
